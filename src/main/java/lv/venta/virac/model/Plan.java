522eaed3700c3cc662412d864b974aca923c19b0
Annija Statevica <annija.statevica@gmail.com>
2025-11-17 21:17:51 +0200

    Update: Model Plan

--- src/main/java/lv/venta/virac/model/Plan.java
+++ src/main/java/lv/venta/virac/model/Plan.java
@@ -1,5 +1,65 @@
 package lv.venta.virac.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Table(name = "planTable")
+@ToString
+@Entity
 public class Plan {
 
+	private int idPlan;
+	
+	//TODO:connect
+	private int idEmployee;
+	
+	//TODO:connect
+	private int idYear;
+	
+	private int numOfProjects;
+	
+	private int numOfArticles;
+	
+	private String partInConf;
+	
+	private String partInConfEnd;
+	
+	private String comAbConf;
+	
+	private String comAbConfEnd;
+	
+	private int numOfCourses;
+	
+	private int numOfStudWork;
+	
+	private String promoOfResearch;
+	
+	private String promoOfResearchEnd;
+	
+	private String adminWork;
+	
+	private String adminWorkEnd;
+	
+	private String projApplicSub;
+	
+	private String projApplicSubEnd;
+	
+	private String skillsDevelopment;
+	
+	private String skillsDevelopmentEnd;
+	
+	private String participationInSeminars;
+	
+	private String participationInSeminarsEnd;
+	
+	private String otherJobs;
+	
+	private String otherJobsEnd;
 }
