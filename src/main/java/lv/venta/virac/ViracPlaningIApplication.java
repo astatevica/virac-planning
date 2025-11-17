1b04ba9707429fbad3252a0bea5ad40c40489185
Annija Statevica <annija.statevica@gmail.com>
2025-11-17 20:24:34 +0200

    First commit

---
+++ src/main/java/lv/venta/virac/ViracPlaningIApplication.java
@@ -0,0 +1,13 @@
+package lv.venta.virac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViracPlaningIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViracPlaningIApplication.class, args);
+	}
+
+}
