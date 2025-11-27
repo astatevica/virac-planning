66e1a26db5a94e3a2efb499ab150373b109b43d9
Annija Statevica <annija.statevica@gmail.com>
2025-11-27 16:08:52 +0200

    Add: added repo ICourseRepo file

---
+++ src/main/java/repo/ICourseRepo.java
@@ -0,0 +1,9 @@
+package repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.virac.model.Course;
+
+public interface ICourseRepo extends CrudRepository<Course, Integer>{
+
+}
