1fdee26b4bc1536890228245e4f1cac88dedfd8a
Annija Statevica <annija.statevica@gmail.com>
2025-11-27 16:24:34 +0200

    Add: added repo IViracDepartmentRepo file

---
+++ src/main/java/repo/IViracDepartmentRepo.java
@@ -0,0 +1,9 @@
+package repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.virac.model.ViracDepartment;
+
+public interface IViracDepartmentRepo extends CrudRepository<ViracDepartment, Integer>{
+
+}
