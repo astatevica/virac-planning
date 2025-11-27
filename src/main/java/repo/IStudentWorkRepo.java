c79d98062023f7e250c97881f8d45ff73f025a35
Annija Statevica <annija.statevica@gmail.com>
2025-11-27 16:23:27 +0200

    Add: added repo IStudentWorkRepo file

---
+++ src/main/java/repo/IStudentWorkRepo.java
@@ -0,0 +1,9 @@
+package repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.virac.model.StudentWork;
+
+public interface IStudentWorkRepo extends CrudRepository<StudentWork, Integer>{
+
+}
