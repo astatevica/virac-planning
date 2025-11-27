76bb8d545be737e08c86dba25208a28b9d89c512
Annija Statevica <annija.statevica@gmail.com>
2025-11-27 16:12:38 +0200

    Add: added repo IEmployeeRepo file

---
+++ src/main/java/repo/IEmployeeRepo.java
@@ -0,0 +1,9 @@
+package repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.virac.model.Employee;
+
+public interface IEmployeeRepo extends CrudRepository<Employee, Integer>{
+
+}
