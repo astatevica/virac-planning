77b75af28cb88877b8246dbdaf91eb4307e38c33
Annija Statevica <annija.statevica@gmail.com>
2025-11-27 16:13:53 +0200

    Add: added repo IYearRepo file

---
+++ src/main/java/repo/IYearRepo.java
@@ -0,0 +1,9 @@
+package repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.virac.model.Year;
+
+public interface IYearRepo extends CrudRepository<Year, Integer>{
+
+}
