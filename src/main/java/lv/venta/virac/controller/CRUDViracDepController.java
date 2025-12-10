db6d5d6ef5dfce5aaf79bafbafb89e3b9542735d
Annija Statevica <annija.statevica@gmail.com>
2025-12-10 14:43:51 +0200

    Add: CRUD ViracDepController

--- src/main/java/lv/venta/virac/controller/CRUDViracDepController.java
+++ src/main/java/lv/venta/virac/controller/CRUDViracDepController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lv.venta.virac.model.ViracDepartment;
@@ -23,12 +24,29 @@ public class CRUDViracDepController {
 	public String getShowAllDepartments(Model model) {
 		try {
 			ArrayList<ViracDepartment> allDepartments = depService.retrieveAll();
-			model.addAttribute("departments",allDepartments);
-			return "departments-show-all-page";
+			model.addAttribute("mydata",allDepartments);
+			return "departments-all-page"; 
 		} catch (Exception e) {
 			model.addAttribute("message", e.getMessage());
 			return "error-page";
 		}
 	}
+	
+	//Retrieve by id
+	@GetMapping("/show/all/{id}")
+	public String getShowOneDepartment(@PathVariable("id") int id, Model model) {
+		try {
+			ViracDepartment department = depService.retrieveById(id);
+			model.addAttribute("mydata",department);
+			return "departments-all-page";
+		} catch (Exception e) {
+			model.addAttribute("message", e.getMessage());
+			return "error-page";
+		}
+	}
+	
+	//Delete by id
+	@GetMapping("/delete/{id}")
+	
 
 }
