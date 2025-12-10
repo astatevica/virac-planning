6db44dd65e99758a1556afe23c393be1068c8bbd
Annija Statevica <annija.statevica@gmail.com>
2025-12-10 14:17:43 +0200

    Add: added service package and CRUD for DepartamentTable

---
+++ src/main/java/lv/venta/virac/service/ICRUDBase.java
@@ -0,0 +1,15 @@
+package lv.venta.virac.service;
+
+import java.util.ArrayList;
+
+public interface ICRUDBase <Ttype>{
+
+		//R - retrieve all
+		public abstract ArrayList<Ttype> retrieveAll() throws Exception;
+		
+		//R - retrieve by id
+		public abstract Ttype retrieveById(int id) throws Exception;
+		
+		//D - delete
+		public abstract void deleteById(int id) throws Exception;
+} 
---
+++ src/main/java/lv/venta/virac/service/ICRUDViracDepService.java
@@ -0,0 +1,14 @@
+package lv.venta.virac.service;
+
+import lv.venta.virac.model.ViracDepartment;
+
+public interface ICRUDViracDepService extends ICRUDBase<ViracDepartment>{
+	//R - retrieve all, R - retrieve by id, D - delete by id būs jau no ICRUDBASE
+	
+	//C - create 
+	public abstract void create(String name) throws Exception;
+	
+	//U - update
+	public abstract void updateById(int id, String name) throws Exception;
+
+}
