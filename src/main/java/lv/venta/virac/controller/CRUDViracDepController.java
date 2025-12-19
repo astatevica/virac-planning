bbe7e2b2adebd1793d7bc0be815c85c355c016bd
Annija Statevica <annija.statevica@gmail.com>
2025-12-19 13:12:08 +0200

    Update: DepController

--- src/main/java/lv/venta/virac/controller/CRUDViracDepController.java
+++ src/main/java/lv/venta/virac/controller/CRUDViracDepController.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
