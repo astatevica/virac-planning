9d903d2bded0206f8c08dbec20ca29dfee102bff
Annija Statevica <annija.statevica@gmail.com>
2026-01-05 07:03:06 +0200

    Add: ICRUDEmployeeService file

--- src/main/java/lv/venta/virac/controller/CRUDViracDepController.java
+++ src/main/java/lv/venta/virac/controller/CRUDViracDepController.java
@@ -2,7 +2,6 @@ package lv.venta.virac.controller;
 
 import java.util.ArrayList;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
---
+++ src/main/java/lv/venta/virac/service/ICRUDEmployeeService.java
@@ -0,0 +1,17 @@
+package lv.venta.virac.service;
+
+import lv.venta.virac.model.Employee;
+import lv.venta.virac.model.ViracDepartment;
+
+public interface ICRUDEmployeeService extends ICRUDBase<Employee>{
+	//R - retrieve all, R - retrieve by id, D - delete by id būs jau no ICRUDBASE
+
+	//C - create 
+	public abstract void create(String name, String surname,
+			ViracDepartment viracDepartment, String position) throws Exception;
+		
+	//U - update
+	public abstract void updateById(int id, String name, String surname,
+			ViracDepartment viracDepartment, String position) throws Exception;
+
+}
