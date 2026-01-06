5d7a3c72b997dd348d9608b3d865665ac7ceb1b8
Annija Statevica <annija.statevica@gmail.com>
2026-01-06 14:51:07 +0200

    Add: Management Implementation

--- src/main/java/lv/venta/virac/service/impl/CRUDProjManagServiceImpl.java
+++ src/main/java/lv/venta/virac/service/impl/CRUDProjManagServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import lv.venta.virac.model.Employee;
 import lv.venta.virac.model.ProjectManagement;
+import lv.venta.virac.model.ViracDepartment;
 import lv.venta.virac.repo.IEmployeeRepo;
 import lv.venta.virac.repo.IProjectManagementRepo;
 import lv.venta.virac.service.ICRUDProjManagService;
@@ -23,31 +24,67 @@ public class CRUDProjManagServiceImpl implements ICRUDProjManagService{
 	
 	@Override
 	public ArrayList<ProjectManagement> retrieveAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<ProjectManagement> projectManag = (ArrayList<ProjectManagement>) managRepo.findAll();
+	       if (projectManag.isEmpty()) throw new Exception("There is no project management");
+
+	        return projectManag;
 	}
 
 	@Override
 	public ProjectManagement retrieveById(int id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if (id < 1) throw new Exception("Invalid ID");
+        ProjectManagement foundManagement = managRepo.findById(id).get();
+        if (foundManagement == null) throw new Exception("Management with the id: (" + id + ") does not exist!");
+        
+        return foundManagement;
 	}
 
 	@Override
 	public void deleteById(int id) throws Exception {
-		// TODO Auto-generated method stub
+		ProjectManagement management = managRepo.findById(id).get();
+    	if (management == null) throw new Exception("Management with id:"+ id +" does not exist");
+        managRepo.delete(management);
 		
 	}
 
+	//Jāpārskata precizitāte
 	@Override
 	public void create(Employee employee, LocalDate startDate, LocalDate endDate) throws Exception {
-		// TODO Auto-generated method stub
+		ArrayList<ProjectManagement> managements = (ArrayList<ProjectManagement>) managRepo.findAll();
+        
+        if(employee == null || startDate == null || endDate == null){
+			throw new Exception("The input parameters are incorrect");
+		}
+        
+        Employee foundEmployee = emplRepo.findById(employee.getIdEmployee())
+                .orElseThrow(() -> new Exception("Employee not found"));
+        
+        for (ProjectManagement manag : managements) {
+            if (manag.getEmployee().equals(foundEmployee) & manag.getStartDate().equals(startDate)
+            		& manag.getEndDate().equals(endDate) ) {
+                throw new Exception("Management: " + manag.getEmployee().getIdEmployee()+ " with start date: " + manag.getStartDate() + 
+                		" and end date : " + manag.getEndDate() + " already exists");
+            }
+        }
+
+        ProjectManagement management = new ProjectManagement(foundEmployee, startDate, endDate);
+        managRepo.save(management);
 		
 	}
 
 	@Override
 	public void updateById(int id, Employee employee, LocalDate startDate, LocalDate endDate) throws Exception {
-		// TODO Auto-generated method stub
+		ProjectManagement management = retrieveById(id);
+    	if (management == null) throw new 
+    		Exception("Management with (id:" + id + ") does not exist");    	
+    	
+    	Employee emp = emplRepo.findById(employee.getIdEmployee())
+                .orElseThrow(() -> new Exception("Employee not found"));
+    	
+        management.setEmployee(employee);
+        management.setStartDate(startDate);
+        management.setEndDate(endDate);
+        managRepo.save(management);
 		
 	}
 
