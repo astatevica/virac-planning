47c6b6aadcb0c9a46737ed2cbac2c7ef5c09eed6
Annija Statevica <annija.statevica@gmail.com>
2026-01-15 14:33:46 +0200

    Add: controller for Plan

--- src/main/java/lv/venta/virac/controller/CRUDPlanController.java
+++ src/main/java/lv/venta/virac/controller/CRUDPlanController.java
@@ -1,9 +1,23 @@
 package lv.venta.virac.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+import lv.venta.virac.dto.PlanDTO;
+import lv.venta.virac.model.Plan;
 import lv.venta.virac.service.ICRUDPlanService;
 
 @RestController
@@ -16,5 +30,158 @@ public class CRUDPlanController {
 	public CRUDPlanController(ICRUDPlanService planService) {
 		this.planService = planService;
 	}
+	
+	@GetMapping
+    public ResponseEntity<ArrayList<PlanDTO>> getAll() throws Exception {
+
+        ArrayList<Plan> plans = planService.retrieveAll();
+
+        ArrayList<PlanDTO> response =
+        	    new ArrayList<>(
+        	    		plans.stream()
+        	            .map(pl -> new PlanDTO(
+        	               pl.getIdPlan(), pl.getEmployee().getIdEmployee(), pl.getYear().getIdYear(),pl.getNumOfProjects(),
+        	               pl.getNumOfArticles(),pl.getPartInConf(),pl.getPartInConfEnd(),pl.getComAbConf(),
+        	               pl.getComAbConfEnd(),pl.getNumOfCourses(),pl.getNumOfStudWork(),pl.getPromoOfResearch(),
+        	               pl.getPromoOfResearchEnd(),pl.getAdminWork(),pl.getAdminWorkEnd(), pl.getProjApplicSub(),
+        	               pl.getProjApplicSubEnd(),pl.getSkillsDevelopment(),pl.getSkillsDevelopmentEnd(),
+        	               pl.getParticipationInSeminars(),pl.getParticipationInSeminarsEnd(),pl.getOtherJobs(),pl.getOtherJobsEnd()
+        	            ))
+        	            .toList()
+        	    );
+        return ResponseEntity.ok(response);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PlanDTO> getById(
+            @PathVariable("id") int id) throws Exception {
+
+        Plan pl = planService.retrieveById(id);
+        System.out.println(pl);
+        return ResponseEntity.ok(
+        		new PlanDTO(
+     	               pl.getIdPlan(), pl.getEmployee().getIdEmployee(), pl.getYear().getIdYear(),pl.getNumOfProjects(),
+     	               pl.getNumOfArticles(),pl.getPartInConf(),pl.getPartInConfEnd(),pl.getComAbConf(),
+     	               pl.getComAbConfEnd(),pl.getNumOfCourses(),pl.getNumOfStudWork(),pl.getPromoOfResearch(),
+     	               pl.getPromoOfResearchEnd(),pl.getAdminWork(),pl.getAdminWorkEnd(), pl.getProjApplicSub(),
+     	               pl.getProjApplicSubEnd(),pl.getSkillsDevelopment(),pl.getSkillsDevelopmentEnd(),
+     	               pl.getParticipationInSeminars(),pl.getParticipationInSeminarsEnd(),pl.getOtherJobs(),pl.getOtherJobsEnd()
+     	            ));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(
+            @Valid @RequestBody PlanDTO pl,
+            BindingResult result) throws Exception {
+
+        if (result.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        planService.create(pl.getIdEmployee(), pl.getIdYear(),pl.getNumOfProjects(),
+	               pl.getNumOfArticles(),pl.getPartInConf(),pl.getPartInConfEnd(),pl.getComAbConf(),
+	               pl.getComAbConfEnd(),pl.getNumOfCourses(),pl.getNumOfStudWork(),pl.getPromoOfResearch(),
+	               pl.getPromoOfResearchEnd(),pl.getAdminWork(),pl.getAdminWorkEnd(), pl.getProjApplicSub(),
+	               pl.getProjApplicSubEnd(),pl.getSkillsDevelopment(),pl.getSkillsDevelopmentEnd(),
+	               pl.getParticipationInSeminars(),pl.getParticipationInSeminarsEnd(),pl.getOtherJobs(),pl.getOtherJobsEnd());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(
+            @PathVariable("id") int id,
+            @Valid @RequestBody PlanDTO pl,
+            BindingResult result) throws Exception {
+
+        if (result.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        planService.updateById(id, pl.getIdEmployee(), pl.getIdYear(),pl.getNumOfProjects(),
+	               pl.getNumOfArticles(),pl.getPartInConf(),pl.getPartInConfEnd(),pl.getComAbConf(),
+	               pl.getComAbConfEnd(),pl.getNumOfCourses(),pl.getNumOfStudWork(),pl.getPromoOfResearch(),
+	               pl.getPromoOfResearchEnd(),pl.getAdminWork(),pl.getAdminWorkEnd(), pl.getProjApplicSub(),
+	               pl.getProjApplicSubEnd(),pl.getSkillsDevelopment(),pl.getSkillsDevelopmentEnd(),
+	               pl.getParticipationInSeminars(),pl.getParticipationInSeminarsEnd(),pl.getOtherJobs(),pl.getOtherJobsEnd());
+        return ResponseEntity.ok().build();
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") int id) throws Exception {
+    	planService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+    
+    @GetMapping("/filter/{idEmployee}")
+    public ResponseEntity<ArrayList<PlanDTO>> selectAllPlansByEmployee(
+            @PathVariable("idEmployee") int idEmployee) throws Exception {
+
+        ArrayList<Plan> plans =
+                planService.selectAllPlansByEmployee(idEmployee);
+
+        ArrayList<PlanDTO> response =
+        	    new ArrayList<>(
+        	    		plans.stream()
+        	            .map(pl -> new PlanDTO(
+        	               pl.getIdPlan(), pl.getEmployee().getIdEmployee(), pl.getYear().getIdYear(),pl.getNumOfProjects(),
+        	               pl.getNumOfArticles(),pl.getPartInConf(),pl.getPartInConfEnd(),pl.getComAbConf(),
+        	               pl.getComAbConfEnd(),pl.getNumOfCourses(),pl.getNumOfStudWork(),pl.getPromoOfResearch(),
+        	               pl.getPromoOfResearchEnd(),pl.getAdminWork(),pl.getAdminWorkEnd(), pl.getProjApplicSub(),
+        	               pl.getProjApplicSubEnd(),pl.getSkillsDevelopment(),pl.getSkillsDevelopmentEnd(),
+        	               pl.getParticipationInSeminars(),pl.getParticipationInSeminarsEnd(),pl.getOtherJobs(),pl.getOtherJobsEnd()
+        	            ))
+        	            .toList()
+        	    );
+        return ResponseEntity.ok(response);
+    }
+    
+    @GetMapping("/filter/{idYear}")
+    public ResponseEntity<ArrayList<PlanDTO>> selectAllPlansByYear(
+            @PathVariable("idYear") int idYear) throws Exception {
+
+        ArrayList<Plan> plans =
+                planService.selectAllPlansByYear(idYear);
+
+        ArrayList<PlanDTO> response =
+        	    new ArrayList<>(
+        	    		plans.stream()
+        	            .map(pl -> new PlanDTO(
+        	               pl.getIdPlan(), pl.getEmployee().getIdEmployee(), pl.getYear().getIdYear(),pl.getNumOfProjects(),
+        	               pl.getNumOfArticles(),pl.getPartInConf(),pl.getPartInConfEnd(),pl.getComAbConf(),
+        	               pl.getComAbConfEnd(),pl.getNumOfCourses(),pl.getNumOfStudWork(),pl.getPromoOfResearch(),
+        	               pl.getPromoOfResearchEnd(),pl.getAdminWork(),pl.getAdminWorkEnd(), pl.getProjApplicSub(),
+        	               pl.getProjApplicSubEnd(),pl.getSkillsDevelopment(),pl.getSkillsDevelopmentEnd(),
+        	               pl.getParticipationInSeminars(),pl.getParticipationInSeminarsEnd(),pl.getOtherJobs(),pl.getOtherJobsEnd()
+        	            ))
+        	            .toList()
+        	    );
+        return ResponseEntity.ok(response);
+    }
+    
+    @GetMapping("/filter/{department}")
+    public ResponseEntity<ArrayList<PlanDTO>> selectAllPlansByDepartment(
+            @PathVariable("department") String department) throws Exception {
+
+        ArrayList<Plan> plans =
+                planService.selectAllPlansByDepartment(department);
+
+        ArrayList<PlanDTO> response =
+        	    new ArrayList<>(
+        	    		plans.stream()
+        	            .map(pl -> new PlanDTO(
+        	               pl.getIdPlan(), pl.getEmployee().getIdEmployee(), pl.getYear().getIdYear(),pl.getNumOfProjects(),
+        	               pl.getNumOfArticles(),pl.getPartInConf(),pl.getPartInConfEnd(),pl.getComAbConf(),
+        	               pl.getComAbConfEnd(),pl.getNumOfCourses(),pl.getNumOfStudWork(),pl.getPromoOfResearch(),
+        	               pl.getPromoOfResearchEnd(),pl.getAdminWork(),pl.getAdminWorkEnd(), pl.getProjApplicSub(),
+        	               pl.getProjApplicSubEnd(),pl.getSkillsDevelopment(),pl.getSkillsDevelopmentEnd(),
+        	               pl.getParticipationInSeminars(),pl.getParticipationInSeminarsEnd(),pl.getOtherJobs(),pl.getOtherJobsEnd()
+        	            ))
+        	            .toList()
+        	    );
+        return ResponseEntity.ok(response);
+    }
 
 }
