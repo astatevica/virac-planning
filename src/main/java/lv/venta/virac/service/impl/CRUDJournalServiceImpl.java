afe664705c5c2a569a1bc99367c4f7929117e808
Annija Statevica <annija.statevica@gmail.com>
2026-05-21 11:36:08 +0300

    Fix: CRUDJournalServiceImpl code smells

--- src/main/java/lv/venta/virac/service/impl/CRUDJournalServiceImpl.java
+++ src/main/java/lv/venta/virac/service/impl/CRUDJournalServiceImpl.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 
 import org.hibernate.Filter;
 import org.hibernate.Session;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -16,11 +17,14 @@ import lv.venta.virac.service.ICRUDJournalService;
 @Service
 public class CRUDJournalServiceImpl implements ICRUDJournalService{
 	
-	@Autowired
 	private IJournalRepo journRepo;
 	
 	@PersistenceContext
     private EntityManager entityManager;
+	
+	public CRUDJournalServiceImpl (IJournalRepo journRepo) {
+		this.journRepo = journRepo;
+	}
 
 	@Override
 	public ArrayList<Journal> retrieveAll() throws Exception {
@@ -28,24 +32,33 @@ public class CRUDJournalServiceImpl implements ICRUDJournalService{
         Filter filter = session.enableFilter("deletedJournalFilter");
         filter.setParameter("isDeleted", false);
         ArrayList<Journal> journals = (ArrayList<Journal>) journRepo.findAll();
-        if (journals.isEmpty()) throw new Exception("There is no journals");
+        if (journals.isEmpty()) throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "There is no journals"
+        );
         session.disableFilter("deletedJournalFilter");
         return journals;
 	}
 
 	@Override
 	public Journal retrieveById(int id) throws Exception {
-		if (id < 1) throw new Exception("Invalid ID");
-        Journal foundJournals = journRepo.findById(id).get();
-        if (foundJournals == null) throw new Exception("Journal with the id: (" + id + ") does not exist!");
-        
-        return foundJournals;
+		if (id < 1) {throw new ResponseStatusException(
+	                HttpStatus.BAD_REQUEST,
+	                "Invalid ID"
+	        );
+		}
+        return journRepo.findById(id).orElseThrow(() -> new ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "Journal with the id: (" + id + ") does not exist!"
+        ));
 	}
 
 	@Override
 	public void deleteById(int id) throws Exception {
-		Journal journal = journRepo.findById(id).get();
-    	if (journal == null) throw new Exception("Journal with id:"+ id +" does not exist");
+		Journal journal = journRepo.findById(id).orElseThrow(() -> new ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "Journal with the id: (" + id + ") does not exist!"
+        ));
     	journal.setDeleted(true); // SOFT DELETE
     	journRepo.save(journal);  // SAVE, NOT DELETE
 		
@@ -56,12 +69,18 @@ public class CRUDJournalServiceImpl implements ICRUDJournalService{
 		ArrayList<Journal> journals = (ArrayList<Journal>) journRepo.findAll();
         
         if(name == null){
-			throw new Exception("The input parameters are incorrect");
+        	throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "The input parameters are incorrect"
+            );
 		}
         
         for (Journal jour : journals) {
-            if (jour.getName().equals(name) & jour.isDeleted( )== false) {
-                throw new Exception("Journal: " + jour.getName() + " already exists");
+            if (jour.getName().equals(name) && jour.isDeleted( )) {
+            	throw new ResponseStatusException(
+                        HttpStatus.BAD_REQUEST,
+                        "Journal: " + jour.getName() + " already exists"
+                );
             }
         }
 
@@ -73,8 +92,10 @@ public class CRUDJournalServiceImpl implements ICRUDJournalService{
 	@Override
 	public void updateById(int id, String name) throws Exception {
 		Journal journal = retrieveById(id);
-    	if (journal == null) throw new 
-    		Exception("Journal with (id:" + id + ") does not exist");
+    	if (journal == null) throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Journal with (id:" + id + ") does not exist"
+        );
     	
     	journal.setName(name);
         journRepo.save(journal);
